9c0deb0e402ae1004d77a1c1b313e75ff75d29c2
Nox2004 <biel.bianchi2004@hotmail.com>
2023-03-28 18:19:57 -0300

    More perspective script fixes

--- Assets/Perspective.cs
+++ Assets/Perspective.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sprite_perspective : MonoBehaviour
+public class Perspective : MonoBehaviour
 {
     struct Edge
     {
